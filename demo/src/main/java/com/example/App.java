7fd8c27f402ed45ffd4c29c3c8b5bb44260c81dd
rodriss11 <79254175+rodriss11sr@users.noreply.github.com>
2023-02-07 15:37:59 +0100

    Cambios rama1

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -14,4 +14,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    public static void funcion(){
+        System.out.println("Hola Mundo");
+    }
 }
